83ba9f42d55a2cee46ee657a7f8454208eb61b1b
Luciano Prado <luciano.junior@citelsoftware.com.br>
2025-11-28 15:31:43 -0300

    Update baseUrl in PedidoService
    Changed baseUrl to a specific endpoint for PedidoService.

--- src/app/core/services/pedido.service.ts
+++ src/app/core/services/pedido.service.ts
@@ -9,7 +9,7 @@ import { environment } from '../../../environments/environment';
   providedIn: 'root'
 })
 export class PedidoService {
-  private baseUrl = environment.apiUrl;
+  private baseUrl ='https://monint.citelsoftware.com.br';
 
   constructor(private http: HttpClient) {}
 
